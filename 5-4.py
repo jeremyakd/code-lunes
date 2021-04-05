90bf4ef2035ee6833b62343ccc0abafc8f5351e1
jeremyakd <jeremyakd@gmail.com>
2021-04-05 20:37:11 -0300

    Add pyhton file

---
+++ 5-4.py
@@ -0,0 +1,13 @@
+# pedimos un nombre por consola y lo imprimimos en la consola
+
+# nombre = input("Hola, ingrese su nombre por favor: ===> ") # funcion de entrada
+
+# print("hola ", nombre) # funcion de salida
+
+# pido 2 numeros y los sumo
+
+num1 = int(input("ingrese un numero por favor."))
+
+num2 = int(input("ingrese un numero por favor."))
+
+print("La suma de los numeros es: ", num1+num2)
