0def07c0ecf2817fd5002615d2b8920fdee89821
alan-hub94 <cereal9494@gmail.com>
2021-04-12 20:44:44 -0300

    primer commit a mi rama

---
+++ 5-4.py
@@ -0,0 +1,10 @@
+#pedimos un nombre y lo imprimimos en la consola.
+
+# print(input("Nombre: "))
+
+#pido 2 numeros y lo sumamos.
+num1 = int(input("numero 1: "))
+num2 = int(input("numero 2: "))
+print("Resultado: " + str(num1 + num2))
+
+
