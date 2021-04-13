dcd33c62d9bafdef0f95c2f09490dcc36cf4b9c0
hitechStart <mdlprofesional@gmail.com>
2021-04-13 19:52:44 -0300

    Se agrego la biblioteca re, funciones, validaciones y estructura repetitiva

--- 5-4.py
+++ 5-4.py
@@ -5,9 +5,35 @@
 # print("hola ", nombre) # funcion de salida
 
 # pido 2 numeros y los sumo
+import re
 
-num1 = int(input("ingrese un numero por favor."))
 
-num2 = int(input("ingrese un numero por favor."))
+def presentacion():
+    print("Programa que permite cargar dos valores por teclado.")
+    print("Efectua la suma de los valores")
+    print("Muestra el resultado de la suma")
+    print("*******************************")
 
-print("La suma de los numeros es: ", num1+num2)
+
+def cargaYSuma():
+
+    while True:
+        try:
+            num1 = int(input("ingrese un numero por favor."))
+            num2 = int(input("ingrese un numero por favor."))
+
+            patron = "[0-9]+"
+
+            if re.search(patron, str(num1)):
+                if re.search(patron, str(num2)):
+                    print("La suma de los numeros es: ", num1+num2)
+                    break
+
+        except ValueError:
+            print("Alguno de los dos valores introducidos no son correctos")
+            print("Intentalo de nuevo.")
+
+# programa principal
+
+presentacion()
+cargaYSuma()
