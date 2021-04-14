c67a45cbe4a8a930b464fced9a83dda78af8f534
hitechStart <mdlprofesional@gmail.com>
2021-04-14 11:12:54 -0300

    Se agrego un vector que contiene los numeros ingresados y se almacenan en un archivo de log cada vez que se ejecute el programa

--- 5-4.py
+++ 5-4.py
@@ -6,34 +6,50 @@
 
 # pido 2 numeros y los sumo
 import re
+import io
+
+# definimos una lista vacia
+numerosIngresados = []
 
 
 def presentacion():
-    print("Programa que permite cargar dos valores por teclado.")
-    print("Efectua la suma de los valores")
-    print("Muestra el resultado de la suma")
-    print("*******************************")
+    print("\n\n\t\tPrograma que permite cargar dos valores por teclado.")
+    print("\t\t\tEfectua la suma de los valores.")
+    print("\t\t\tMuestra el resultado de la suma.")
+    print("\t\t*********************************************************\n\n")
 
 
 def cargaYSuma():
 
     while True:
         try:
-            num1 = int(input("ingrese un numero por favor."))
-            num2 = int(input("ingrese un numero por favor."))
+            num1 = int(input("ingrese el primer numero: "))
+            numerosIngresados.append(str(num1))
+            num2 = int(input("ingrese el segundo numero:"))
+            numerosIngresados.append(str(num2))
 
             patron = "[0-9]+"
 
             if re.search(patron, str(num1)):
                 if re.search(patron, str(num2)):
-                    print("La suma de los numeros es: ", num1+num2)
+                    print("La suma de los numeros es: ", num1 + num2)
                     break
-
         except ValueError:
             print("Alguno de los dos valores introducidos no son correctos")
             print("Intentalo de nuevo.")
+
+def creacionDeArchivoDeLog():
+
+    file=open("Log.txt","a") 
+    file.write(str(numerosIngresados))
+    file.write("\n")
+    file.close()
+
+
 
 # programa principal
 
 presentacion()
 cargaYSuma()
+creacionDeArchivoDeLog()
+
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # code-lunes
 Còdigo de las clases
-Modificado 13/04/2021 18:54 hs 
+Modificado 13/04/2021 18:54 hs
+Modificado 14/04/2021 11:11 hs
+
